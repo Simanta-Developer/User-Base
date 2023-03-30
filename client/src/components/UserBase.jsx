763e629f7b875e482dd8d9668e988b2eb356f93f
Simanta-Developer <85247063+Simanta-Developer@users.noreply.github.com>
2023-03-30 18:04:24 +0530

    Initial commit

---
+++ client/src/components/UserBase.jsx
@@ -0,0 +1,12 @@
+import { styled } from '@mui/material' ;
+
+
+
+const UserBase = () =>{
+    return(
+        <h1>Welcome to the User Base application website</h1>
+        
+    ) 
+}
+
+export default UserBase ;
---
+++ server/database/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose" ;
+
+
+
+const Connection = async(username, password) => {
+    const URL = `mongodb+srv://${username}:${password}@crudapp.lug8iiu.mongodb.net/?retryWrites=true&w=majority` ;
+    try{
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true })
+        console.log("database connected sucessfully") ;
+    }catch(error){
+        console.log("Error while connecting with the database", error) ;
+    }
+}
+
+export default Connection ;
+
+
+
