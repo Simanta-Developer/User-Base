89a6b70f933ab4d13000fddd8d3230b5c2cdafc7
Simanta-Developer <85247063+Simanta-Developer@users.noreply.github.com>
2023-08-20 22:36:00 +0530

    env corrected

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose" ;
 
 
 const Connection = async(username, password) => {
-    const URL = `mongodb+srv://${username}:${password}@crudapp.lug8iiu.mongodb.net/?retryWrites=true&w=majority` ;
+    const URL = `mongodb+srv://simantaraj45:sonuNoSql@crudapp.lug8iiu.mongodb.net/?retryWrites=true&w=majority` ;
     try{
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true })
         console.log("database connected sucessfully") ;
